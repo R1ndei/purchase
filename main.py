ed048f3a308ce9d1a9f273a33335b5f79cd05a44
R1ndei <steam.denis2017@yandex.ru>
2022-07-05 18:48:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import time
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+from fastapi.background import BackgroundTasks
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+redis = get_redis_connection(
+    host=os.getenv('HOST'),
+    port=os.getenv('PORT'),
+    password=os.getenv('PASSWORD'),
+    decode_responses=True
+)
+
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str
+
+    class Meta:
+        database = redis
+
+
+@app.get("/orders/{pk}")
+def get(pk: str):
+    return Order.get(pk)
+
+
+@app.post("/orders")
+async def create(request: Request, background_tasks: BackgroundTasks):
+    body = await request.json()
+    req = requests.get("http://localhost:8000/products/%s" % body['id'])
+    product = req.json()
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 + product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+    order.save()
+
+    background_tasks.add_task(order_complete, order)
+
+    return order
+
+
+def order_complete(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
+    redis.xadd("order_completed", order.dict(), '*')
